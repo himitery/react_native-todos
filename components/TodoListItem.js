e82e54814ccf22779de45ae1ddc740efd66b718a
himitery <himit0131@naver.com>
2021-02-21 21:59:56 +0900

    React Native Tutorial: 아이콘 추가

--- components/TodoListItem.js
+++ components/TodoListItem.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const TodoListItem = () => {
   return (
     <View style={styles.container}>
       <TouchableOpacity>
-        <View style={styles.circle} />
+        <View style={styles.completeCircle}>
+          <Icon name="circledowno" size={30} color="#3143e8" />
+        </View>
       </TouchableOpacity>
-      <Text style={styles.text}>TodoList items will be shown here</Text>
+      <Text style={(styles.text, styles.strikeText)}>
+        Items will be shown here
+      </Text>
     </View>
   );
 };
@@ -36,6 +41,21 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     marginRight: 20,
     marginLeft: 20,
+  },
+  completeCircle: {
+    marginRight: 20,
+    marginLeft: 20,
+  },
+  strikeText: {
+    color: '#bbb',
+    textDecorationLine: 'line-through',
+  },
+  unstrikeText: {
+    color: '#29323c',
+  },
+  buttonContainer: {
+    marginVertical: 10,
+    marginHorizontal: 10,
   },
 });
 
